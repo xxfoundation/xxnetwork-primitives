180f6c05dae7c69db52e551e9a622ed6a0f8f225
Benjamin Wenger <ben@elixxir.ioo>
2019-02-26 15:42:31 -0800

    Missed a use of crypto/hash

--- format/registrationCode_test.go
+++ format/registrationCode_test.go
@@ -10,6 +10,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"gitlab.com/elixxir/crypto/hash"
+	"golang.org/x/crypto/blake2b"
 	"math"
 	"reflect"
 	"testing"
@@ -56,7 +57,7 @@ func TestRegistrationHash(t *testing.T) {
 	}
 	copy(regcode[len(regcode)-len(regBytes):], regBytes)
 
-	hasher, _ := hash.NewCMixHash()
+	hasher, _ := blake2b.New256(nil)
 
 	hasher.Write(regcode)
 
