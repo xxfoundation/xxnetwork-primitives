f7777c92872f7fc2fc417b2298b5cac3bf4e2047
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-03-13 12:59:17 -0700

    Seperating payload test from field_test and renaming field_test to associatedData_test

---
+++ format/payload_test.go
@@ -0,0 +1,37 @@
+package format
+
+import "testing"
+
+
+// Test each field of the payload
+func TestPayload(t *testing.T) {
+	payload := NewPayload()
+	var err error
+	err = testField(payload.GetSenderID, payload.SetSenderID,
+		payload.SerializePayload, MP_SID_LEN)
+	if err != nil {
+		t.Errorf("Sender ID failed: %v", err.Error())
+	}
+	err = testField(payload.GetSender().Bytes, payload.SetSenderID,
+		payload.SerializePayload, MP_SID_LEN)
+	if err != nil {
+		t.Errorf("Sender ID by id.User failed: %v", err.Error())
+	}
+	// These functions return variable length according to size of actual data
+	// so must be tested with different function
+	err = testVariableField(payload.GetPayloadData, payload.SetPayloadData,
+		payload.SerializePayload, MP_PAYLOAD_LEN)
+	if err != nil {
+		t.Errorf("Payload Data failed: %v", err.Error())
+	}
+	err = testVariableField(payload.GetPayload, payload.SetPayload,
+		payload.SerializePayload, TOTAL_LEN)
+	if err != nil {
+		t.Errorf("Payload failed: %v", err.Error())
+	}
+	err = testVariableField(payload.GetPayload, payload.SetSplitPayload,
+		payload.SerializePayload, MP_PAYLOAD_LEN)
+	if err != nil {
+		t.Errorf("Payload Split failed: %v", err.Error())
+	}
+}
