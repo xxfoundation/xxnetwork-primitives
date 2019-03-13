4d555fff15098e286ea69bc73a4a616ab5fb2e92
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-03-13 14:31:57 -0700

    Finished refactoring tests

--- format/associatedData_test.go
+++ format/associatedData_test.go
@@ -12,7 +12,6 @@ import (
 	"fmt"
 	"gitlab.com/elixxir/primitives/id"
 	"math/rand"
-	"reflect"
 	"testing"
 )
 
@@ -203,39 +202,5 @@ func TestAssociatedData_Timestamp(t *testing.T) {
 		data.SerializeAssociatedData, AD_TIMESTAMP_LEN)
 	if err != nil {
 		t.Errorf("Timestamp failed: %v", err.Error())
-	}
-}
-
-func TestDeepCopy(t *testing.T) {
-	// Generate test data for each structure
-	r := rand.New(rand.NewSource(0))
-	testBytes := make([]byte, TOTAL_LEN)
-	_, err := r.Read(testBytes)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	// Make a deep copy of each structure, and make sure that changing one
-	// doesn't change the other
-	data := DeserializeAssociatedData(testBytes)
-	dataCopy := data.DeepCopy()
-	payload := DeserializePayload(testBytes)
-	payloadCopy := payload.DeepCopy()
-
-	if !reflect.DeepEqual(data, dataCopy) {
-		t.Error("Datas should have been equal before mutation, but weren't")
-	}
-	if !reflect.DeepEqual(payload, payloadCopy) {
-		t.Error("Payloads should have been equal before mutation, but weren't")
-	}
-	// Mutate each copy. The originals and copies should now be different
-	dataCopy.SetRecipient(id.NewUserFromUint(5, t))
-	payloadCopy.SetSender(id.NewUserFromUint(5, t))
-	if reflect.DeepEqual(data, dataCopy) {
-		t.Error("Datas should have been different after mutation, but weren't")
-	}
-	if reflect.DeepEqual(payload, payloadCopy) {
-		t.Error("Payloads should have been different after mutation, " +
-			"but weren't")
 	}
 }
