cac8039e478590922727f6cba414e4326336a975
Bernardo Cardoso <bernardo@elixxir.io>
2019-04-10 14:55:10 -0600

    Add String function to CryptoType

--- format/outer.go
+++ format/outer.go
@@ -11,3 +11,25 @@ const (
 	Error
 	Rekey
 )
+
+// Return string representation
+// of CryptoType
+// Might be useful in tests and debug prints
+func (ct CryptoType) String() string {
+	var ret string
+	switch ct {
+	case None:
+		ret = "None"
+	case Unencrypted:
+		ret = "Unencrypted"
+	case E2E:
+		ret = "E2E"
+	case Garbled:
+		ret = "Garbled"
+	case Error:
+		ret = "Error"
+	case Rekey:
+		ret = "Rekey"
+	}
+	return ret
+}
