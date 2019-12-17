78eaebdb32b57ab0c2dc3dcc7553e555f4f02144
Konstantino Sparakis <ksparakis@elixxir.io>
2019-12-17 01:35:07 -0500

    formated project

--- switchboard/listenersMap_test.go
+++ switchboard/listenersMap_test.go
@@ -7,11 +7,8 @@ import (
 	"testing"
 )
 
-
-
-
 func TestListenersMap_StoreListener(t *testing.T) {
-	sb, mockListener:= OneListenerSetup()
+	sb, mockListener := OneListenerSetup()
 	newListenerRecord := &listenerRecord{
 		l:  mockListener,
 		id: strconv.Itoa(sb.lastID),
@@ -22,22 +19,20 @@ func TestListenersMap_StoreListener(t *testing.T) {
 	mapKey := listenerMapKey{id.ZeroID, int32(5)}
 	records_i, ok := sb.listenersMap.listeners.Load(mapKey)
 	records := records_i.([]*listenerRecord)
-	if !ok{
+	if !ok {
 		t.Logf("Store listener failed, could not find stored object")
 		t.Fail()
 	}
 
 	testRecords := []*listenerRecord{newListenerRecord}
-	if !reflect.DeepEqual(records, testRecords){
+	if !reflect.DeepEqual(records, testRecords) {
 		t.Logf("Record found does not match record initially stored")
 		t.Fail()
 	}
 }
 
-
-
 func TestListenersMap_GetListenerRecords(t *testing.T) {
-	sb, mockListener:= OneListenerSetup()
+	sb, mockListener := OneListenerSetup()
 	newListenerRecord := &listenerRecord{
 		l:  mockListener,
 		id: strconv.Itoa(sb.lastID),
@@ -46,20 +41,20 @@ func TestListenersMap_GetListenerRecords(t *testing.T) {
 	sb.listenersMap.StoreListener(id.ZeroID, int32(5), newListenerRecord)
 
 	records, ok := sb.listenersMap.GetListenerRecords(id.ZeroID, int32(5))
-	if !ok{
+	if !ok {
 		t.Logf("Get Listener Records failed, could not find stored object")
 		t.Fail()
 	}
 
 	testRecords := []*listenerRecord{newListenerRecord}
-	if !reflect.DeepEqual(records, testRecords){
+	if !reflect.DeepEqual(records, testRecords) {
 		t.Logf("Get Listener Records returned wrong object")
 		t.Fail()
 	}
 }
 
 func TestListenersMap_GetMatches(t *testing.T) {
-	sb, mockListener:= OneListenerSetup()
+	sb, mockListener := OneListenerSetup()
 	listenerId := strconv.Itoa(sb.lastID)
 	newListenerRecord := &listenerRecord{
 		l:  mockListener,
@@ -71,23 +66,22 @@ func TestListenersMap_GetMatches(t *testing.T) {
 
 	//Test that matches are appended
 	matchesTestA := sb.listenersMap.GetMatches(matches, id.ZeroID, int32(5))
-	if reflect.DeepEqual(matches, matchesTestA){
+	if reflect.DeepEqual(matches, matchesTestA) {
 		t.Logf("Matches were not found when they should have been")
 		t.Fail()
 	}
 
 	//Test that if you pass in the same variables duplicates don't occur
 	matchesTestB := sb.listenersMap.GetMatches(matchesTestA, id.ZeroID, int32(5))
-	if !reflect.DeepEqual(matchesTestB, matchesTestA){
+	if !reflect.DeepEqual(matchesTestB, matchesTestA) {
 		t.Logf("Duplicates found in matches")
 		t.Fail()
 	}
-	
+
 }
 
-
 func TestListenersMap_RemoveListener(t *testing.T) {
-	sb, mockListener:= OneListenerSetup()
+	sb, mockListener := OneListenerSetup()
 	listenerId := strconv.Itoa(sb.lastID)
 	newListenerRecord := &listenerRecord{
 		l:  mockListener,
@@ -99,9 +93,9 @@ func TestListenersMap_RemoveListener(t *testing.T) {
 	sb.listenersMap.RemoveListener(listenerId)
 
 	records, ok := sb.listenersMap.GetListenerRecords(id.ZeroID, int32(5))
-	if ok{
+	if ok {
 		testRecords := []*listenerRecord{newListenerRecord}
-		if reflect.DeepEqual(records, testRecords){
+		if reflect.DeepEqual(records, testRecords) {
 			t.Logf("Failed to remove listener from ListenersMap")
 			t.Fail()
 		}
