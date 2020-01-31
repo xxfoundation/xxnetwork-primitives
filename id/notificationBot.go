cde195e3ff2b284c77dde3bef483a2244a86e00e
jbhusson <jonah@elixxir.io>
2020-01-31 10:16:05 -0800

    add headers

--- id/notificationBot.go
+++ id/notificationBot.go
@@ -1,3 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+// ID constant for notifications
+
 package id
 
 const NOTIFICATION_BOT = "notification-bot"
