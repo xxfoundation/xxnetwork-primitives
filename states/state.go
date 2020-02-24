71e96d0bbfb719f40dbd7313bd144865b553f14c
Benjamin Wenger <ben@elixxir.ioo>
2020-02-24 10:08:55 -0800

    changed package name to states

--- states/state.go
+++ states/state.go
@@ -4,7 +4,7 @@
 // All rights reserved.                                                        /
 ////////////////////////////////////////////////////////////////////////////////
 
-package state
+package states
 
 import "fmt"
 
