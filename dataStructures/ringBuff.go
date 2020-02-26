964e4031639867acfa847556fbc1ae9d78c62081
jbhusson <jonah@elixxir.io>
2020-02-26 15:03:24 -0800

    rename first, last, len fields

--- dataStructures/ringBuff.go
+++ dataStructures/ringBuff.go
@@ -9,32 +9,32 @@ type idFunc func(interface{}) int
 type compFunc func(interface{}, interface{}) bool
 
 type RingBuff struct {
-	buff             []interface{}
-	len, first, last int
-	id               idFunc
-	lock             sync.Mutex
+	buff              []interface{}
+	count, head, tail int
+	id                idFunc
+	lock              sync.Mutex
 }
 
 // next is a helper function for ringbuff
-// it handles incrementing the first & last markers
+// it handles incrementing the head & tail markers
 func (rb *RingBuff) next() {
-	rb.last = (rb.last + 1) % rb.len
-	if rb.last == rb.first {
-		rb.first = (rb.first + 1) % rb.len
+	rb.tail = (rb.tail + 1) % rb.count
+	if rb.tail == rb.head {
+		rb.head = (rb.head + 1) % rb.count
 	}
-	if rb.first == -1 {
-		rb.first = 0
+	if rb.head == -1 {
+		rb.head = 0
 	}
 }
 
 // getIndex is a helper function for ringbuff
-// it returns an index relative to the first/last position of the buffer
+// it returns an index relative to the head/tail position of the buffer
 func (rb *RingBuff) getIndex(i int) int {
 	var index int
 	if i < 0 {
-		index = (rb.last + rb.len + i) % rb.len
+		index = (rb.tail + rb.count + i) % rb.count
 	} else {
-		index = (rb.first + i) % rb.len
+		index = (rb.head + i) % rb.count
 	}
 	return index
 }
@@ -43,9 +43,9 @@ func (rb *RingBuff) getIndex(i int) int {
 func New(n int, id idFunc) *RingBuff {
 	rb := &RingBuff{
 		buff:  make([]interface{}, n),
-		len:   n,
-		first: -1,
-		last:  0,
+		count: n,
+		head:  -1,
+		tail:  0,
 		id:    id,
 	}
 	return rb
@@ -53,7 +53,7 @@ func New(n int, id idFunc) *RingBuff {
 
 // Push a round to the buffer
 func (rb *RingBuff) push(val interface{}) {
-	rb.buff[rb.last] = val
+	rb.buff[rb.tail] = val
 	rb.next()
 }
 
@@ -71,7 +71,7 @@ func (rb *RingBuff) UpsertById(val interface{}, comp compFunc) error {
 	defer rb.lock.Unlock()
 	newId := rb.id(val)
 
-	if rb.id(rb.buff[rb.first]) > newId {
+	if rb.id(rb.buff[rb.head]) > newId {
 		return errors.Errorf("Did not upsert value %+v; id is older than first tracked", val)
 	}
 
@@ -95,12 +95,12 @@ func (rb *RingBuff) UpsertById(val interface{}, comp compFunc) error {
 }
 
 func (rb *RingBuff) Get() interface{} {
-	mostRecentIndex := (rb.last + rb.len - 1) % rb.len
+	mostRecentIndex := (rb.tail + rb.count - 1) % rb.count
 	return rb.buff[mostRecentIndex]
 }
 
 func (rb *RingBuff) GetById(id int) (interface{}, error) {
-	firstId := rb.id(rb.buff[rb.first])
+	firstId := rb.id(rb.buff[rb.head])
 	if id < firstId {
 		return nil, errors.Errorf("requested ID %d is lower than oldest id %d", id, firstId)
 	}
@@ -115,5 +115,5 @@ func (rb *RingBuff) GetById(id int) (interface{}, error) {
 }
 
 func (rb *RingBuff) Len() int {
-	return rb.len
+	return rb.count
 }
