d3515b7eb377ecfa5b0f5c329f41c10755dff5a0
jaketaylor <jake@privategrity.com>
2020-03-09 09:55:38 -0700

    renamed WAITING to PENDING

--- states/states_test.go
+++ states/states_test.go
@@ -5,11 +5,11 @@ import "testing"
 //tests the test stringer is correct
 func TestActivity_String(t *testing.T) {
 	//define some states to check
-	expectedStateStringer := []string{"WAITING", "PRECOMPUTING", "STANDBY",
+	expectedStateStringer := []string{"PENDING", "PRECOMPUTING", "STANDBY",
 		"REALTIME", "COMPLETED", "FAILED", "UNKNOWN STATE: 6"}
 
 	//check if states give the correct return
-	for st := WAITING; st <= NUM_STATES; st++ {
+	for st := PENDING; st <= NUM_STATES; st++ {
 		if st.String() != expectedStateStringer[st] {
 			t.Errorf("Round %d did not string correctly: expected: %s,"+
 				"recieved: %s", uint8(st), expectedStateStringer[st], st.String())
