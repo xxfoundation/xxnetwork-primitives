ea8539d1681f8a2974895555b0b5858b051424f9
Benjamin Wenger <ben@elixxir.ioo>
2020-08-25 17:50:47 -0700

    fixed a metadata leak in the message structure

--- format/message.go
+++ format/message.go
@@ -13,30 +13,39 @@ import (
 )
 
 const (
-	GrpByteLen     = 1
-	RecipientIDLen = 33
+	SizeLen        = 2
 	KeyFPLen       = 32
-	TimestampLen   = 16
 	MacLen         = 32
+	RecipientIDLen = 33
+	TimestampLen   = 16
 
-	// Length of the entire message serial
-	AssociatedDataLen = RecipientIDLen + KeyFPLen + TimestampLen + MacLen
+	MinimumPrimeSize = 2*MacLen + RecipientIDLen + TimestampLen
+
+	AssociatedDataSize = KeyFPLen + SizeLen + MacLen + RecipientIDLen + TimestampLen
 )
 
 /*                               Message Structure (not to scale)
-+----------------------------------------------------------------------------------------+
-|                                         Message                                        |
-|                                        4096 bits                                       |
-+----------------------------------------------------------------------------------------+
-|                  payloadA                  |                 payloadB                  |
-|                 2048 bits                  |                2048 bits                  |
-+------------------------------------+-------+---------------------------------+---------+
-|              Contents              |             AssociatedData              | grpByte |
-|              3192 bits             |                896 bits                 | 8 bits  |
-+------------------------------------+-----------------------------------------+         |
-|     padding     |       data       | recipientID | keyFP | timestamp |  mac  |         |
-|   88–3192 bits  |    0–3104 bits   |   264 bits  | 256 b |  128 bits | 256 b |         |
-+-----------------+------------------+-------------+-------+-----------+-------+---------+
++-----------------------------------------------------------------------------------------------------------+
+|                                                Message                                                    |
+|                                            2*primeSize bits                                               |
++---------------------------------------------------+-------------------------------------------------------+
+|                  payloadA                         |                 payloadB                              |
+|               primeSize bits                      |              primeSize bits                           |
++---------+----------+---------+--------------------+---------+-------+-----------+-------------+-----------+
+| grpBitA |  keyFP   |  size   |     Contents1      | grpBitB |  mac  | Contents2 | recipientID | timestamp |
+|  1 bit  | 255 bits | 16 bits |      *below*       | 1 bit   | 255 b |  *below*  |  264 bits   |   128 b   |
++ --------+----------+---------+--------------------+---------+-------+-----------+-------------+-----------+
+
+size - size in bits of the data which is stored
+
+Size Contents1 = primeSize - grpBitASize - keyFPSize - sizeSize
+Size Contents2 = primeSize - grpBitBSize - macSize- recipientIDSize - timestampSize
+
+the size of the data in the two contents fields is stored within the "size" field
+
+/////Adherence to the group/////////////////////////////////////////////////////
+The first bits of keyFingerprint and MAC are enforced to be 0, thus ensuring
+PayloadA and PayloadB are within the group
 */
 
 // Message structure stores all the data serially. Subsequent fields point to
@@ -48,29 +57,26 @@ type Message struct {
 	payloadA []byte
 	payloadB []byte
 
-	groupByteA  []byte
+	keyFP       []byte
+	size        []byte
 	contents1   []byte
-	groupByteB  []byte
+	mac         []byte
 	contents2   []byte
 	recipientID []byte
-	keyFP       []byte
 	timestamp   []byte
-	mac         []byte
-
-	associatedData []byte
 }
 
-// NewMessage creates a new empty message. It points the contents, associated
-// data, payload A, and payload B, to their respective parts of master.
+// NewMessage creates a new empty message based upon the size of the encryption
+// primes. All subcomponents point to locations in the internal data buffer.
+// Panics if the prime size to too small.
 func NewMessage(numPrimeBytes int) Message {
-
-	if numPrimeBytes < 2*(AssociatedDataLen+GrpByteLen) {
-		panic("cannot make message based off of to small prime")
+	if numPrimeBytes < MinimumPrimeSize {
+		jww.FATAL.Panicf("cannot make message based off of a prime of "+
+			"size %v, minnimum size is %v to small prime", numPrimeBytes,
+			MinimumPrimeSize)
 	}
 
 	data := make([]byte, 2*numPrimeBytes)
-
-	adStart := 2*numPrimeBytes - AssociatedDataLen - GrpByteLen
 
 	return Message{
 		data: data,
@@ -78,19 +84,30 @@ func NewMessage(numPrimeBytes int) Message {
 		payloadA: data[:numPrimeBytes],
 		payloadB: data[numPrimeBytes:],
 
-		groupByteA:  data[0:1],
-		contents1:   data[1:numPrimeBytes],
-		groupByteB:  data[numPrimeBytes : numPrimeBytes+1],
-		contents2:   data[numPrimeBytes+1 : adStart],
-		recipientID: data[adStart : adStart+RecipientIDLen],
-		keyFP:       data[adStart+RecipientIDLen : adStart+RecipientIDLen+KeyFPLen],
-		timestamp:   data[adStart+RecipientIDLen+KeyFPLen : adStart+RecipientIDLen+KeyFPLen+MacLen],
+		keyFP:     data[0:KeyFPLen],
+		size:      data[KeyFPLen : KeyFPLen+SizeLen],
+		contents1: data[KeyFPLen+SizeLen:],
 
-		associatedData: data[adStart : adStart+RecipientIDLen+KeyFPLen+MacLen],
+		mac:         data[numPrimeBytes : numPrimeBytes+MacLen],
+		contents2:   data[numPrimeBytes+MacLen : 2*numPrimeBytes-RecipientIDLen-TimestampLen],
+		recipientID: data[2*numPrimeBytes-RecipientIDLen-TimestampLen : 2*numPrimeBytes-TimestampLen],
+		timestamp:   data[2*numPrimeBytes-TimestampLen:],
 	}
 }
 
-// GetMaster returns the entire serialised message.
+// Returns a copy of the message
+func (m Message) Copy() Message {
+	m2 := NewMessage(len(m.data) / 2)
+	copy(m2.data, m.data)
+	return m2
+}
+
+// Returns the maximum size of the contents
+func (m Message) ContentsSize() int {
+	return len(m.data) - AssociatedDataSize
+}
+
+// Returns the underlying data buffer of the message
 func (m Message) GetData() []byte {
 	return copyByteSlice(m.data)
 }
@@ -129,6 +146,8 @@ func (m Message) SetPayloadB(payload []byte) {
 	copy(m.payloadB, payload)
 }
 
+// Get contents returns the exact contents of the message. This size of the
+// return is based on the size of the contents actually stored
 func (m Message) GetContents() []byte {
 	size := int(m.getContentsSize())
 	c := make([]byte, size)
@@ -143,6 +162,11 @@ func (m Message) GetContents() []byte {
 	return c
 }
 
+// sets the contents of the message. This will panic if the payload is greater
+// than the maximum size. This overwrites any storage already in the message but
+// will not clear bits beyond the size of the passed contents.
+// If the passed contents is larger than the maximum contents size this will
+// panic
 func (m Message) SetContents(c []byte) {
 	if len(c) > len(m.contents1)+len(m.contents2) {
 		jww.ERROR.Panicf("contents too large at %v bytes, must be "+
@@ -155,36 +179,80 @@ func (m Message) SetContents(c []byte) {
 		copy(m.contents1, c[:len(m.contents1)])
 		copy(m.contents2, c[len(m.contents1):])
 	}
+
+	m.setContentsSize(uint16(len(c)))
 }
 
+// Returns the maximum size of the contents
+func (m Message) GetSecretPayloadSize() int {
+	return SizeLen + len(m.contents1) + len(m.contents2)
+}
+
+// Gets the entire payload which needs to be end to end encrypted for the
+// communication to be secret. Contains the size, Contents1 and Contents2
+func (m Message) GetSecretPayload() []byte {
+	sp := make([]byte, SizeLen+len(m.contents1)+len(m.contents2))
+	copy(sp[:SizeLen], m.size)
+	copy(sp[SizeLen:SizeLen+len(m.contents1)], m.contents1)
+	copy(sp[SizeLen+len(m.contents1):SizeLen+len(m.contents1)+len(m.contents2)], m.contents2)
+	return sp
+}
+
+// Sets the entire payload which needs to be end to end encrypted for the
+// communication to be secret. Sets the size, Contents1 and Contents2. Must be
+// the exact size of the SecretPayload.
+func (m Message) SetSecretPayload(sp []byte) {
+	if len(sp) != m.GetSecretPayloadSize() {
+		jww.ERROR.Panicf("secretPayload wrong size at %v bytes, must be "+
+			"%v bytes", len(sp), m.GetSecretPayloadSize())
+	}
+
+	copy(m.size, sp[:SizeLen])
+	copy(m.contents1, sp[SizeLen:SizeLen+len(m.contents1)])
+	copy(m.contents2, sp[SizeLen+len(m.contents1):SizeLen+len(m.contents1)+len(m.contents2)])
+}
+
+// Gets the recipientID
 func (m Message) GetRecipientID() *id.ID {
 	rid := id.ID{}
 	copy(rid[:], m.recipientID)
 	return &rid
 }
 
+// Sets the recipientID
 func (m Message) SetRecipientID(rid *id.ID) {
 	copy(m.recipientID, rid[:])
 }
 
-func (m Message) GetKeyFP() []byte {
-	return copyByteSlice(m.keyFP)
+// Gets the Key Fingerprint
+func (m Message) GetKeyFP() Fingerprint {
+	fp := Fingerprint{}
+	copy(fp[:], m.keyFP)
+	return fp
 }
 
-func (m Message) SetKeyFP(fp []byte) {
+// Sets the Key Fingerprint. Checks that the first bit of the Key Fingerprint is
+// 0, otherwise it panics.
+func (m Message) SetKeyFP(fp Fingerprint) {
 	if len(fp) != len(m.keyFP) {
 		jww.ERROR.Panicf("key fingerprint not the correct size;"+
 			"Expected: %v, Recieved: %v",
 			len(m.keyFP), len(fp))
 	}
 
-	copy(m.keyFP, fp)
+	if fp[0]>>7 != 0 {
+		jww.ERROR.Panicf("key fingerprint's first bit is not zero")
+	}
+
+	copy(m.keyFP, fp[:])
 }
 
+// Gets the timestamp
 func (m Message) GetTimestamp() []byte {
 	return copyByteSlice(m.timestamp)
 }
 
+// Sets the timestamp. Panics if the passed data is not the exact right size
 func (m Message) SetTimestamp(ts []byte) {
 	if len(ts) != len(m.timestamp) {
 		jww.ERROR.Panicf("timestamp not the correct size;"+
@@ -195,39 +263,39 @@ func (m Message) SetTimestamp(ts []byte) {
 	copy(m.timestamp, ts)
 }
 
+// Gets the MAC
 func (m Message) GetMac() []byte {
 	return copyByteSlice(m.mac)
 }
 
+// Sets the MAC. Checks that the first bit of the MAC is
+// 0, otherwise it panics.
 func (m Message) SetMac(mac []byte) {
 	if len(mac) != len(m.mac) {
-		jww.ERROR.Panicf("timestamp not the correct size;"+
+		jww.ERROR.Panicf("mac is not the correct size;"+
 			"Expected: %v, Recieved: %v",
 			len(m.mac), len(mac))
+	}
+
+	if mac[0]>>7 != 0 {
+		jww.ERROR.Panicf("mac's first bit is not zero")
 	}
 
 	copy(m.mac, mac)
 }
 
 /*private functions*/
+// Sets the size of the contents
 func (m Message) setContentsSize(s uint16) {
-	sEnc := make([]byte, 2)
-	binary.BigEndian.PutUint16(sEnc, s)
-
-	m.groupByteB[0] = sEnc[1] & 0x7f
-	m.groupByteA[0] = (sEnc[0]&0x3f)<<1 | (sEnc[1]&0x80)>>7
+	binary.BigEndian.PutUint16(m.size, s)
 }
 
+//gets the size of the contents
 func (m Message) getContentsSize() uint16 {
-	sEnc := make([]byte, 2)
-
-	sEnc[1] = m.groupByteB[0] | (m.groupByteA[0]&0x1)<<7
-	sEnc[0] = (m.groupByteA[0] & 0x7E) >> 1
-
-	return binary.BigEndian.Uint16(sEnc)
-
+	return binary.BigEndian.Uint16(m.size)
 }
 
+//helper function to copy a byte slice
 func copyByteSlice(s []byte) []byte {
 	c := make([]byte, len(s))
 	copy(c, s)
