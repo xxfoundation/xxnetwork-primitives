75a81252d7e050af907e1636c0e2d541d14f4d8a
jbhusson <jonah@elixxir.io>
2021-01-06 16:35:30 -0500

    fix tests

--- id/ephemeral/id_test.go
+++ id/ephemeral/id_test.go
@@ -1,6 +1,7 @@
 package ephemeral
 
 import (
+	"gitlab.com/xx_network/crypto/csprng"
 	"gitlab.com/xx_network/primitives/id"
 	_ "golang.org/x/crypto/blake2b"
 	"testing"
@@ -9,11 +10,11 @@ import (
 // Unit test for GetId
 func TestGetId(t *testing.T) {
 	testId := id.NewIdFromString("zezima", id.User, t)
-	eid, err := GetId(testId, 99)
+	eid, err := GetId(testId, 99, csprng.NewSystemRNG())
 	if err == nil {
 		t.Error("Should error with size > 64")
 	}
-	eid, err = GetId(testId, 16)
+	eid, err = GetId(testId, 16, csprng.NewSystemRNG())
 	if err != nil {
 		t.Errorf("Failed to create ephemeral ID: %+v", err)
 	}
