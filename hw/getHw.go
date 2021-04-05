33d8051569a9574a32484be727017099342a8c82
Sydney Anne Erickson <sydney@elixxir.io>
2021-04-05 11:33:25 -0700

    change errors

--- hw/getHw.go
+++ hw/getHw.go
@@ -9,6 +9,8 @@
 package hw
 
 import (
+	"errors"
+	"fmt"
 	"os/exec"
 
 	jww "github.com/spf13/jwalterweatherman"
@@ -18,50 +20,50 @@ func LogHardware() error {
 	// lscpu
 	out, err := exec.Command("lscpu").Output()
     if err != nil {
-        return err
+        return errors.New(fmt.Sprintf("lscpu: %s", err))
     }
 	jww.INFO.Printf("[HWINFO] CPU INFO:\r\n%s", out)
 
 	// lspci GPUs
 	out, err = exec.Command("bash", "-c", "lspci -vnnn | perl -lne 'print if /^\\d+\\:.+(\\[\\S+\\:\\S+\\])/' | grep VGA").Output()
     if err != nil {
-        return err
+        return errors.New(fmt.Sprintf("lspci gpu: %s", err))
     }
 	jww.INFO.Printf("[HWINFO] GPU INFO:\r\n%s", out)
 
 	// lsblk
 	out, err = exec.Command("lsblk").Output()
     if err != nil {
-        return err
+        return errors.New(fmt.Sprintf("lsblk: %s", err))
     }
 	jww.INFO.Printf("[HWINFO] PARTITION INFO:\r\n%s", out)
 
 	// df disk usage
 	out, err = exec.Command("df", "-h").Output()
     if err != nil {
-        return err
+        return errors.New(fmt.Sprintf("df: %s", err))
     }
 	jww.INFO.Printf("[HWINFO] DISK USAGE INFO:\r\n%s", out)
 
 	// disk hw info
 	out, err = exec.Command("lshw", "-class", "disk", "-class", "storage").Output()
 	if err != nil {
-		return err
-	}
+        return errors.New(fmt.Sprintf("lshw: %s", err))
+    }
 	jww.INFO.Printf("[HWINFO] DISK HW INFO:\r\n%s", out)
 
 	// RAM info
 	out, err = exec.Command("dmidecode", "--type", "17").Output()
     if err != nil {
-        return err
+        return errors.New(fmt.Sprintf("dmidecode: %s", err))
     }
 	jww.INFO.Printf("[HWINFO] RAM HW INFO:\r\n%s", out)
 
 	// RAM usage
 	out, err = exec.Command("free", "-mt").Output()
 	if err != nil {
-		return err
-	}
+        return errors.New(fmt.Sprintf("free: %s", err))
+    }
 	jww.INFO.Printf("[HWINFO] RAM USAGE INFO:\r\n%s", out)
 
 	return nil
