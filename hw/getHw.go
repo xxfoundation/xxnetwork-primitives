beb38f9b647fd721cb635332d02c4d78ed476093
josh <josh@elixxir.io>
2021-08-02 10:44:04 -0700

    Improve printing

--- hw/getHw.go
+++ hw/getHw.go
@@ -20,54 +20,56 @@ func LogHardware() error {
 	jww.INFO.Printf("---------------------Printing Hardware Information---------------------")
 	// lscpu
 	out, err := exec.Command("lscpu").Output()
-    if err != nil {
-        return errors.New(fmt.Sprintf("lscpu: %s", err))
-    }
+	if err != nil {
+		return errors.New(fmt.Sprintf("lscpu: %s", err))
+	}
 	jww.INFO.Printf("---------------------CPU INFO---------------------:\r\n%s", out)
 
 	// lspci GPUs
 	out, err = exec.Command("bash", "-c", "lspci -vnnn | perl -lne 'print if /^\\d+\\:.+(\\[\\S+\\:\\S+\\])/' | grep VGA").Output()
-    if err != nil {
-        return errors.New(fmt.Sprintf("lspci gpu: %s", err))
-    }
+	if err != nil {
+		return errors.New(fmt.Sprintf("lspci gpu: %s", err))
+	}
 	jww.INFO.Printf("---------------------GPU INFO---------------------:\r\n%s", out)
 
 	// lsblk
 	out, err = exec.Command("lsblk").Output()
-    if err != nil {
-        return errors.New(fmt.Sprintf("lsblk: %s", err))
-    }
+	if err != nil {
+		return errors.New(fmt.Sprintf("lsblk: %s", err))
+	}
 	jww.INFO.Printf("---------------------PARTITION INFO---------------------:\r\n%s", out)
 
 	// df disk usage
 	out, err = exec.Command("df", "-h").Output()
-    if err != nil {
-        return errors.New(fmt.Sprintf("df: %s", err))
-    }
+	if err != nil {
+		return errors.New(fmt.Sprintf("df: %s", err))
+	}
 	jww.INFO.Printf("---------------------DISK USAGE INFO---------------------:\r\n%s", out)
 
 	// disk hw info
 	out, err = exec.Command("lshw", "-class", "disk", "-class", "storage").Output()
 	if err != nil {
-        return errors.New(fmt.Sprintf("lshw: %s", err))
-    }
+		return errors.New(fmt.Sprintf("lshw: %s", err))
+	}
 	jww.INFO.Printf("---------------------DISK HW INFO---------------------:\r\n%s", out)
 
 	/* requires root :(
-	// RAM info
-	out, err = exec.Command("dmidecode", "--type", "17").Output()
-    if err != nil {
-        return errors.New(fmt.Sprintf("dmidecode: %s", err))
-    }
-	jww.INFO.Printf("[HWINFO] RAM HW INFO:\r\n%s", out)
+		// RAM info
+		out, err = exec.Command("dmidecode", "--type", "17").Output()
+	    if err != nil {
+	        return errors.New(fmt.Sprintf("dmidecode: %s", err))
+	    }
+		jww.INFO.Printf("[HWINFO] RAM HW INFO:\r\n%s", out)
 	*/
 
 	// RAM usage
 	out, err = exec.Command("free", "-mt").Output()
 	if err != nil {
-        return errors.New(fmt.Sprintf("free: %s", err))
-    }
+		return errors.New(fmt.Sprintf("free: %s", err))
+	}
 	jww.INFO.Printf("---------------------RAM USAGE INFO---------------------:\r\n%s", out)
+
+	jww.INFO.Printf("---------------------End of Hardware Information---------------------")
 
 	return nil
 }
