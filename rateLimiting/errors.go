636c7b31cb7886189d6c01389ec8eab3d3a89b86
josh <josh@elixxir.io>
2022-02-14 14:54:57 -0800

    Add constant rate limiting error

---
+++ rateLimiting/errors.go
@@ -0,0 +1,6 @@
+package rateLimiting
+
+// Error messages to return once a user has hit the rate limti
+const (
+	ClientRateLimitErr = "Too many messages sent from ID %v with IP address %s in a specific time frame by a user"
+)
