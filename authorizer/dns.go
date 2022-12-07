90d589f4bfae36fe55f652d3ec6fec7ebb1f96d8
Jake Taylor <jake@elixxir.io>
2022-12-07 12:27:46 -0600

    increase length of dns

--- authorizer/dns.go
+++ authorizer/dns.go
@@ -12,12 +12,20 @@ import (
 	"fmt"
 )
 
-// DomainName is the registered domain to be used for calculation of
-// unique Gateway DNS addresses by authorizer, gateway, and client.
-const DomainName = "xxnode.io"
+const (
+	// DomainName is the registered domain to be used for calculation of
+	// unique Gateway DNS addresses by authorizer, gateway, and client.
+	DomainName = "xxnode.io"
+	// Maximum length of DNS name. Determined by third party service.
+	maxLength = 64
+	// Maximum number of characters of gateway ID to use. Subtract length of
+	// domain plus the additional period from maxLength.
+	maxGwIdLength = maxLength - len(DomainName) - 1
+)
 
 // GetGatewayDns returns the DNS name for the given marshalled GwId.
+// Truncates
 func GetGatewayDns(gwId []byte) string {
 	return fmt.Sprintf("%s.%s",
-		base64.URLEncoding.EncodeToString(gwId)[:20], DomainName)
+		base64.URLEncoding.EncodeToString(gwId)[:maxGwIdLength], DomainName)
 }
