5d104598093ecad29323050031081f511aa12509
Jono Wenger <jono@elixxir.io>
2023-07-03 11:37:03 -0700

    Fix comments and tests for idList

--- id/idList_test.go
+++ id/idList_test.go
@@ -8,29 +8,22 @@
 package id
 
 import (
-	"bytes"
 	"math/rand"
+	"strings"
 	"testing"
 )
 
 func TestNewIDListFromBytes(t *testing.T) {
-	// Topology list will contain the list of strings to be passed to
-	// NewIDListFromBytes
-	var topologyList [][]byte
+	prng := rand.New(rand.NewSource(5856532))
 
-	// ExpectedNodes will contain the constructed IDs, to be compared one-by-one
-	// to the output of NewIDListFromBytes
-	var expectedIDs []*ID
-
-	// Construct a topology list
+	// Construct a topology list and a list of matching IDs
+	n := 10
+	expectedIDs := make([]*ID, n)
+	topologyList := make([][]byte, n)
 	for i := 0; i < 10; i++ {
-		// Construct an ID
-		idBytes := newRandomBytes(ArrIDLen, t)
-		expectedId := NewIdFromBytes(idBytes, t)
-
-		// Append to the slices
-		expectedIDs = append(expectedIDs, expectedId)
-		topologyList = append(topologyList, expectedId.Bytes())
+		expectedId := NewRandomTestID(prng, User, t)
+		expectedIDs[i] = expectedId
+		topologyList[i] = expectedId.Bytes()
 	}
 
 	// Pass topologyList into NewIDListFromBytes
@@ -41,50 +34,31 @@ func TestNewIDListFromBytes(t *testing.T) {
 
 	// Iterate through the list and comparing receivedIDs to expectedIDs every
 	// iteration
-	for index, receivedID := range receivedIDs {
-		expectedNode := expectedIDs[index]
-
+	for i, receivedID := range receivedIDs {
 		// Check the outputted list to the expected values
-		if !bytes.Equal(receivedID.Bytes(), expectedNode.Bytes()) {
+		if !receivedID.Equal(expectedIDs[i]) {
 			t.Errorf("ID of index %d was not converted correctly."+
-				"\nreceived: %v\nexpected: %v", index, receivedID.Bytes(),
-				expectedNode.Bytes())
+				"\nreceived: %s\nexpected: %s", i, receivedID, expectedIDs[i])
 		}
-
 	}
 
 }
 
-// Error path: construct a list with a bad topology
+// Error path: construct a list with a bad topology.
 func TestNewIDListFromBytes_Error(t *testing.T) {
-	// Topology list will contain the list of strings to be passed to
-	// NewIDListFromBytes
-	var topologyList [][]byte
+	prng := rand.New(rand.NewSource(5856532))
 
-	// ExpectedNodes will contain the constructed nodes, to be compared
-	// one-by-one to the output of NewIDListFromBytes
-	var expectedNodes []*ID
-
-	// Construct a topology list
-	for i := 0; i < 10; i++ {
-		// construct id
-		idBytes := newRandomBytes(ArrIDLen, t)
-		expectedId := NewIdFromBytes(idBytes, t)
-		expectedNodes = append(expectedNodes, expectedId)
-
-		// Inject a bad byte slices into the list of slices at an arbitrary point
-		if i == rand.Int()%5 {
-			expectedIdBytes := []byte{1, 2, 3}
-			topologyList = append(topologyList, expectedIdBytes)
-		}
+	topologyList := [][]byte{
+		NewRandomTestID(prng, User, t).Bytes(),
+		[]byte("invalid ID"),
+		NewRandomTestID(prng, User, t).Bytes(),
 	}
 
 	// Attempt to convert the topologyList
 	_, err := NewIDListFromBytes(topologyList)
-	if err != nil {
-		return
+	if err == nil || !strings.Contains(err.Error(), "failed to unmarshal ID") {
+		t.Errorf("NewIDListFromBytes did not return an error when an invalid " +
+			"ID is in the list.")
 	}
 
-	t.Errorf("Expected error case, should not successfully create a list" +
-		"of nodes due to a bad topology")
 }
