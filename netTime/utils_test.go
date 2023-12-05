537c186e5f45880cf4fbd4fbaa77cb8d85a540a3
Richard T. Carback III <rick.carback@gmail.com>
2023-12-05 18:48:53 +0000

    Update copyright headers for 2024

--- netTime/utils_test.go
+++ netTime/utils_test.go
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                          //
+// Copyright © 2024 xx foundation                                             //
 //                                                                           //
 // Use of this source code is governed by a license that can be found in the //
 // LICENSE file                                                              //
