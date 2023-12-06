f7c71b05e13e619feabdc078678d9eb8ff2def3c
Richard T. Carback III <rick.carback@gmail.com>
2023-12-06 01:50:27 +0000

    Update copyright

--- LICENSE
+++ LICENSE
@@ -1,4 +1,4 @@
-Copyright (c) 2020, xx network SEZC
+Copyright © 2024 xx foundation
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
--- states/states_test.go
+++ states/states_test.go
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright © 2022 xx foundation                                             //
+// Copyright © 2024 xx foundation                                             //
 //                                                                            //
 // Use of this source code is governed by a license that can be found in the  //
 // LICENSE file.                                                              //
